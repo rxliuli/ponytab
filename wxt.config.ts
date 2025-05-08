8f14a80ea39790bbd120b992d8455b270faa3996
rxliuli <rxliuli@gmail.com>
2025-05-08 17:35:38 +0800

    chore: support firefox&safari

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,21 +1,31 @@
-import { defineConfig } from 'wxt'
+import { defineConfig, UserManifest } from 'wxt'
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifestVersion: 3,
-  manifest: {
-    name: 'PonyTab',
-    description:
-      'A beautiful Chrome extension that replaces your new tab page with stunning My Little Pony artwork by Sam Baneko.',
-    permissions: ['storage', 'alarms'],
-    host_permissions: ['https://ponytab.rxliuli.com/*'],
-    chrome_url_overrides: {
-      newtab: 'newtab.html',
-    },
-    author: {
-      email: 'rxliuli@gmail.com',
-    },
-    homepage_url: 'https://rxliuli.com/project/ponytab',
+  manifest: (env) => {
+    const manifest: UserManifest = {
+      name: 'PonyTab',
+      description:
+        'A beautiful Chrome extension that replaces your new tab page with stunning My Little Pony artwork by Sam Baneko.',
+      permissions: ['storage', 'alarms'],
+      host_permissions: ['https://ponytab.rxliuli.com/*'],
+      chrome_url_overrides: {
+        newtab: 'newtab.html',
+      },
+      author: {
+        email: 'rxliuli@gmail.com',
+      },
+      homepage_url: 'https://rxliuli.com/project/ponytab',
+    }
+    if (env.browser === 'firefox') {
+      manifest.browser_specific_settings = {
+        gecko: {
+          id: 'ponytab@rxliuli.com',
+        },
+      }
+    }
+    return manifest
   },
   webExt: {
     disabled: true,
